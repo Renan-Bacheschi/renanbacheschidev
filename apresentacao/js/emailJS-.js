82a63084e973c131463c63dda740fadbb07ed915
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2025-08-22 14:40:04 -0300

    atualizando para validacao com REGEX

--- apresentacao/js/emailJS-.js
+++ apresentacao/js/emailJS-.js
@@ -1,26 +1,5 @@
-    document.addEventListener('DOMContentLoaded', function() {
-        emailjs.init('xNJssMe-789Hh2ywx');
+document.addEventListener('DOMContentLoaded', function(){
 
-        const contatoFormulario = document.getElementById('contato-formulario');
-        const statusFormulario = document.getElementById('status-formulario');
-
-        contatoFormulario.addEventListener('submit', function(event) {
-            event.preventDefault();
-
-            statusFormulario.textContent = 'Enviando...';
-            statusFormulario.style.color = '#333';
-
-            emailjs.sendForm('service_adbxu6c', 'template_utwglwx', this)
-                .then(function(response) {
-                    console.log('SUCESSO!', response.status, response.text);
-                    statusFormulario.textContent = 'Mensagem enviada com sucesso!';
-                    statusFormulario.style.color = 'green';
-                    contatoFormulario.reset();
-                }, function(error) {
-                    console.log('FALHOU...', error);
-                    statusFormulario.textContent = 'Ocorreu um erro. Tente Novamente.';
-                    statusFormulario.style.color = 'red';
-                });
-        });
-    });
-
+    //Chaves e Ids EmailJS
+    const PUBLIC_KEY = 'xNJssMe-789Hh2ywx';
+})
