246f39e2eacf923df7660c1a56d7ab491dae9d14
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2025-08-22 16:29:06 -0300

    estrutura typping effect ok, configurar tempo, cores e final deixar estatico

---
+++ apresentacao/js/type-effects.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const options = {
+        strings: [
+            " Renan. ",
+            " Desenvolvedor Front-end.",
+            "Criador de soluções Web."
+        ],
+
+        typeSpeed: 70,
+
+        backSpeed: 50,
+
+        loop: false
+    };
+
+    const typed = new Typed('#typing-effect', options);
+});
